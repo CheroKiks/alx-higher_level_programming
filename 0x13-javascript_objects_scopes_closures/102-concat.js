b5dab0b7122ccf8d33a96cbccaa06d2a047958f7
CheroKiks <cherotichirine@gmail.com>
2023-09-14 05:48:17 +0300

    JS tasks update

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
 
-//modules
-const fs = require('fs').promises;
-const { argv } = require('process');
-
-fs.readFile(argv[2], 'utf8')
-  .then(data => fs.writeFile(argv[4], data, 'utf8'))
-  .catch(e => console.error(e));
-
-fs.readFile(argv[3], 'utf8')
-  .then(data => fs.writeFile(argv[4], data, { flag: 'a' }, 'utf8'))
-  .catch(e => console.error(e));
+const fs = require('fs');
+const src1 = fs.readFileSync(process.argv[2], 'utf8');
+const src2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], src1 + src2);
